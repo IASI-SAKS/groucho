6b97b9bd2bc61824770cb981b194a548dbc41302
Gulyx <guglielmo.deangelis@isti.cnr.it>
2019-03-29 19:46:55 +0100

    Added another implementation for the GovernanceManager that will actually implement the logic we are investigating

---
+++ groucho-core/src/main/java/it/cnr/iasi/saks/groucho/callback/SmarterGovernanceManager.java
@@ -0,0 +1,44 @@
+/* 
+ * This file is part of the GROUCHO project.
+ * 
+ * GROUCHO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * GROUCHO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GROUCHO.  If not, see <https://www.gnu.org/licenses/>
+ *
+ */
+package it.cnr.iasi.saks.groucho.callback;
+
+import it.cnr.iasi.saks.groucho.common.Context;
+import it.cnr.iasi.saks.groucho.common.StateCarver;
+
+public class SmarterGovernanceManager extends SimpleGovernanceManager {
+
+	@Override
+	public boolean evaluateActivation(Context context) {
+		StateCarver carver = new StateCarver(context);
+		try {
+			carver.carveAllFields();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+
+}
