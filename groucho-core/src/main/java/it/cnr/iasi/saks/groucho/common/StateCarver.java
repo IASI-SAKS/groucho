39774fbfaba4441e4268e3535341af94f51f78cb
Gulyx <guglielmo.deangelis@isti.cnr.it>
2019-04-05 17:55:47 +0200

    Improved the StateCarver so that is support Arrays and it better support both primitives and complex types

--- groucho-core/src/main/java/it/cnr/iasi/saks/groucho/common/StateCarver.java
+++ groucho-core/src/main/java/it/cnr/iasi/saks/groucho/common/StateCarver.java
@@ -17,6 +17,7 @@
  */
 package it.cnr.iasi.saks.groucho.common;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
@@ -26,23 +27,30 @@ import java.util.List;
 
 public class StateCarver {
 
-	private static final String JAVA_STRING = "java.lang.String";
-
-	private static final String TOKEN_SEPARATOR_LAVEL_VALUE = "-->";
-	private static final String TOKEN_OBJECT_START = "[";
-	private static final String TOKEN_OBJECT_END = "]";
+	private static final String TOKEN_SEPARATOR_LEVEL_VALUE = "-->";
+	private static final String TOKEN_ARRAY_START = "[";
+	private static final String TOKEN_ARRAY_END = "]";
+	private static final String TOKEN_ELEMENT_SEPARATOR = "ç";
+	private static final String TOKEN_OBJECT_START = "{";
+	private static final String TOKEN_OBJECT_END = "}";
 	private static final String TOKEN_FIELD_SEPARATOR = "%";
 	
 	private Object instrumentedObject;
 	private String invivoTestClassName;
 	private int maxCarvingDepth;
 	
+	private boolean processFinalFieldsFlag;
+
 	public StateCarver(Context context){
 		if (context != null){
 			this.invivoTestClassName = context.getInstrumentedClassName();
 			this.instrumentedObject = context.getInstrumentedObject();
 			this.maxCarvingDepth = context.getMaxCarvingDepth();
+		}else{
+			this.maxCarvingDepth = 0;			
+			this.invivoTestClassName = "--Not-Initialized-Yet--";
 		}
+		this.processFinalFieldsFlag = false;
 	}
 	
 /**
@@ -55,25 +63,14 @@ public class StateCarver {
  * @throws ClassNotFoundException
  */
 	public String carveAllFields() throws IllegalArgumentException, ClassNotFoundException {
-			Class<?> clazz = Class.forName(this.invivoTestClassName, false, ClassLoader.getSystemClassLoader()); 
-            Field[] fields = clazz.getDeclaredFields();
-            
-/**
- * TODO We may consider to order the attribute of a class by name
- */
-          List<String> orderedFieldNames = this.orderedFieldByName(fields);
+		String carvedState = "";
+		if (this.maxCarvingDepth > 0) {
+			carvedState = this.flattenInteralState(this.instrumentedObject, this.maxCarvingDepth);
+		} else {
+			carvedState = TOKEN_OBJECT_START + TOKEN_OBJECT_END;
+		}
 
-        	String carvedState = TOKEN_OBJECT_START;
-            synchronized (this.instrumentedObject) {
-                if (this.maxCarvingDepth > 0){
-                	for (Field field : fields) {
-                		carvedState = carvedState + TOKEN_FIELD_SEPARATOR + this.flattenInteralState(field, this.instrumentedObject, this.maxCarvingDepth);
-                	}
-                }
-            }
-            carvedState = carvedState.replaceFirst(TOKEN_FIELD_SEPARATOR, "") + TOKEN_OBJECT_END;
-     	    
-            return carvedState;
+		return carvedState;
 	}
 		    
 /**
@@ -83,34 +80,87 @@ public class StateCarver {
  *
  * @throws IllegalArgumentException 
 */
-	private String flattenInteralState(Field field, Object flatteingObject, int actualCarvingDepth) throws IllegalArgumentException {
-		System.out.println("Name: "+field.getName()+", Object Type:"+flatteingObject.getClass().getName());
-		String currentFlattenedState = "";
-		if (actualCarvingDepth > 0) {
-			Class<?> clazz = field.getType();
-			Object fieldValue = dumpFieldValue(field, flatteingObject);
-			if (this.isComplexType(clazz)) {
-				String localFlattening = TOKEN_OBJECT_START;
-				for (Field f : clazz.getDeclaredFields()) {					
-					String tmp = this.flattenInteralState(f, fieldValue, actualCarvingDepth - 1);
+	private String flattenInteralState(Object flatteingObject, int actualCarvingDepth) throws IllegalArgumentException {
+		Field[] fields = null;
 
-					if (!tmp.isEmpty()){
-						if (tmp.startsWith(TOKEN_OBJECT_START)){
-							tmp = f.getName() + TOKEN_SEPARATOR_LAVEL_VALUE + tmp;
-						}	
-						localFlattening = localFlattening + TOKEN_FIELD_SEPARATOR + tmp;
+		if (flatteingObject != null) {
+			System.out.println("Object Type:" + flatteingObject.getClass().getName());
+			fields = flatteingObject.getClass().getDeclaredFields();
+
+			/**
+			 * TODO We may consider to order the attribute of a class by name
+			 */
+			List<String> orderedFieldNames = this.orderedFieldByName(fields);
+
+		} else {
+			System.out.println("Object Type: it is null!!!");
+		}
+		String currentFlattenedState = "";
+		if (this.isSpecialComplexType(flatteingObject)) {
+			currentFlattenedState = flatteingObject.toString();
+		} else {
+			if (actualCarvingDepth > 0) {
+				currentFlattenedState = TOKEN_OBJECT_START;
+				for (Field field : fields) {
+					if (this.checkFieldProcessing(field)) {
+						Class<?> fieldType = field.getType();
+						System.out.println("Processing Field: " + field.getName() + " Object Type:"
+								+ fieldType.getCanonicalName());
+						Object fieldValue = dumpFieldValue(field, flatteingObject);
+						if (fieldValue != null) {
+							Class<?> instanceType = fieldValue.getClass();
+							if (this.isComplexType(fieldType)) {
+								String localFlattening = flattenInteralState(fieldValue, actualCarvingDepth - 1);
+								if (localFlattening.isEmpty()) {
+									localFlattening = TOKEN_ARRAY_START + TOKEN_ARRAY_END;
+								}
+								currentFlattenedState = currentFlattenedState + TOKEN_FIELD_SEPARATOR + field.getName()
+										+ TOKEN_SEPARATOR_LEVEL_VALUE + localFlattening;
+							} else {
+								if (fieldType.isArray()) {
+									String localFlattening = this.processArrays(fieldValue, actualCarvingDepth);
+									currentFlattenedState = currentFlattenedState + TOKEN_FIELD_SEPARATOR
+											+ field.getName() + TOKEN_SEPARATOR_LEVEL_VALUE + localFlattening;
+								} else {
+									currentFlattenedState = currentFlattenedState + TOKEN_FIELD_SEPARATOR
+											+ field.getName() + TOKEN_SEPARATOR_LEVEL_VALUE + fieldValue;
+								}
+							}
+						}
 					}
 				}
-				localFlattening = localFlattening.replaceFirst(TOKEN_FIELD_SEPARATOR, "");
-
-				currentFlattenedState = field.getName() + TOKEN_SEPARATOR_LAVEL_VALUE + localFlattening + TOKEN_OBJECT_END;
-			} else {
-				currentFlattenedState = field.getName() + TOKEN_SEPARATOR_LAVEL_VALUE + fieldValue;
+				currentFlattenedState = currentFlattenedState.replaceFirst(TOKEN_FIELD_SEPARATOR, "")
+						+ TOKEN_OBJECT_END;
 			}
 		}
+
 		return currentFlattenedState;
 	}
-	
+
+	private String processArrays(Object flatteingObject, int actualCarvingDepth) {
+		String currentFlattenedState = TOKEN_ARRAY_START;
+		if ((actualCarvingDepth > 0) && (flatteingObject != null)) {
+
+			String localFlattening = "";
+			int length = Array.getLength(flatteingObject);
+			for (int indexCounter = 0; indexCounter < length; indexCounter++) {
+				Object obj = Array.get(flatteingObject, indexCounter);
+				String tmp = "";
+				if (obj != null) {
+					Class<?> clazz = obj.getClass();
+//					this.processFinalFieldsFlag = true;
+					tmp = this.flattenInteralState(obj, actualCarvingDepth - 1);
+					localFlattening = localFlattening + TOKEN_ELEMENT_SEPARATOR + tmp;
+//					this.processFinalFieldsFlag = false;
+				}
+			}
+			localFlattening = localFlattening.replaceFirst(TOKEN_ELEMENT_SEPARATOR, "");
+			currentFlattenedState = currentFlattenedState + localFlattening;
+		}
+		currentFlattenedState = currentFlattenedState + TOKEN_ARRAY_END;
+		return currentFlattenedState;
+	}
+
 	private List<String> orderedFieldByName(Field[] fields){
 		List<String> listOfFieldNames = new ArrayList<String>();
 		Collections.sort(listOfFieldNames);
@@ -118,9 +168,37 @@ public class StateCarver {
 	}
 	
 	private boolean isComplexType(Class<?> c){
-		boolean result = !((c.isPrimitive()) || (c.isEnum()) || (c.getTypeName().equalsIgnoreCase(JAVA_STRING)));
-
+		boolean result = false;
+		if ( c != null ){
+			result = !((c.isPrimitive()) || (c.isEnum()) || (c.isArray()));
+		}	
 		return result;
+	}
+
+	private boolean isSpecialComplexType(Object o){
+		boolean result = (o instanceof String) || (o instanceof Number);
+		
+		return result;
+	}
+
+	/**
+	 * The processing of a field is subject to an implication between its final modifier and the flag that enables the processing of final attributes.
+	 * More specifically:
+	 * processIt := isFinal --> this.processFinalFieldsFlag
+	 *    true		 false				false
+	 *    true		 false				true
+	 *    false		 true				false
+	 *    true		 true				true
+	 **/
+	private boolean checkFieldProcessing(Field f){
+		boolean isFinal = (f != null) && (Modifier.isFinal(f.getModifiers())); 
+
+		/**
+		 * processIt := isFinal --> this.processFinalFieldsFlag
+		 */
+		boolean processIt = (! isFinal) || this.processFinalFieldsFlag; 
+		
+		return processIt;
 	}
 	
 /**
@@ -132,24 +210,25 @@ public class StateCarver {
  * @throws IllegalArgumentException
  */
 	private Object dumpFieldValue(Field f, Object instace) throws IllegalArgumentException{
-		Object fieldValue = null; 
-		boolean wasPrivate = false;
-        if (Modifier.isPrivate(f.getModifiers())) {
-        	wasPrivate = true;
-     	    f.setAccessible(true);
-        }
+		Object fieldValue = null;
+		boolean wasPublic = true;
+		if (!Modifier.isPublic(f.getModifiers())) {
+			wasPublic = false;
+			f.setAccessible(true);
+		}
 
-        try {
+		try {
 			fieldValue = f.get(instace);
-		} catch (IllegalAccessException e) {
+		} catch (NullPointerException | IllegalAccessException e) {
+			e.printStackTrace();
 			fieldValue = null;
 		}
 
-        if (wasPrivate) {
-     	    f.setAccessible(false);
-        }
-        
-        return fieldValue; 
+		if (!wasPublic) {
+			f.setAccessible(false);
+		}
+
+		return fieldValue;
 	}
 	
 }
