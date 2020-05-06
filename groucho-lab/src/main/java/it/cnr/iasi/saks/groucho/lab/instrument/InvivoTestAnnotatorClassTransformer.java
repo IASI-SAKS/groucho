4f5b01287d9dc4ce53d608bd770dde7ef91d36ae
Gulyx <guglielmo.deangelis@isti.cnr.it>
2020-05-07 00:53:48 +0200

    removed duplicated and useless lines

--- groucho-lab/src/main/java/it/cnr/iasi/saks/groucho/lab/instrument/InvivoTestAnnotatorClassTransformer.java
+++ groucho-lab/src/main/java/it/cnr/iasi/saks/groucho/lab/instrument/InvivoTestAnnotatorClassTransformer.java
@@ -20,51 +20,17 @@ package it.cnr.iasi.saks.groucho.lab.instrument;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
-import java.util.Iterator;
-import java.util.List;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 
-import it.cnr.iasi.saks.groucho.config.PropertyUtil;
-
 public class InvivoTestAnnotatorClassTransformer implements ClassFileTransformer {
 	
-	private boolean isDisabled(String className) {
-		return (this.isClassNameIgnoredByCrochet(className) || this.isLocallyIgnored(className));
-	}
-
-	private boolean isLocallyIgnored(String className) {
-		boolean exitus = className.startsWith("java/");
-		exitus = exitus || className.startsWith("sun/");
-		exitus = exitus || className.startsWith("it/cnr/iasi/saks/groucho/");
-		exitus = exitus || className.startsWith("org/junit/");
-		exitus = exitus || className.startsWith("org/apache/maven/");
-		exitus = exitus || className.startsWith("org/eclipse/jdt/internal/junit/");
-		
-		if (!exitus) {
-			List<String> excludeClassesList = PropertyUtil.getInstance().getClassesToExcludeDuringTransformation();
-			for (Iterator<String> iterator = excludeClassesList.iterator(); iterator.hasNext() && !exitus;) {
-				String classToExclude = (String) iterator.next();
-				exitus = exitus || className.startsWith(classToExclude);
-
-			}
-		}
-		return exitus;
-	}
-
-	private boolean isClassNameIgnoredByCrochet(String className) {
-		 boolean exitus = net.jonbell.crij.instrument.Instrumenter.isIgnoredClass(className);
-		 exitus = exitus || net.jonbell.crij.instrument.Instrumenter.isIgnoredClassWithDummyMethods(className);
-		 return exitus;
-	}
-
 	/*
 	 * (non-Javadoc) It is invoked for every class loaded into the JVM. It
 	 * provides the byte array of the class as input to the method, which then
-	 * returns the modified byte array. If the class transformer decides not to
-	 * modify the bytes of the specific class, then it can return null.
-	 * 
+	 * returns the modified byte array. 
+	 * 	 
 	 * @see java.lang.instrument.ClassFileTransformer#transform(java.lang.
 	 * ClassLoader, java.lang.String, java.lang.Class,
 	 * java.security.ProtectionDomain, byte[])
@@ -72,9 +38,6 @@ public class InvivoTestAnnotatorClassTransformer implements ClassFileTransformer
 	@Override
 	public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
 			ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-		if (this.isDisabled(className)) {
-			return null;
-		}
 
 		System.out.println("Currently applying the tranformation on: " + className);
 		
