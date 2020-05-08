d7b17087078ba4f8cbb5a069087dacbbd9250b1d
Gulyx <guglielmo.deangelis@isti.cnr.it>
2020-05-08 14:57:25 +0200

    Added generalized concpect of ClassTransformer adding the new AbstractClassTransformer class. The GouchoClassTransformer now extends the AbstractClassTransformer

--- groucho-core/src/main/java/it/cnr/iasi/saks/groucho/instrument/GrouchoClassTransformer.java
+++ groucho-core/src/main/java/it/cnr/iasi/saks/groucho/instrument/GrouchoClassTransformer.java
@@ -17,48 +17,14 @@
  */
 package it.cnr.iasi.saks.groucho.instrument;
 
-import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
-import java.util.Iterator;
-import java.util.List;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 
-import it.cnr.iasi.saks.groucho.config.PropertyUtil;
-
-public class GrouchoClassTransformer implements ClassFileTransformer {
+public class GrouchoClassTransformer extends AbstractClassTranformer {
 	
-	private boolean isDisabled(String className) {
-		return (this.isClassNameIgnoredByCrochet(className) || this.isLocallyIgnored(className));
-	}
-
-	private boolean isLocallyIgnored(String className) {
-		boolean exitus = className.startsWith("java/");
-		exitus = exitus || className.startsWith("sun/");
-		exitus = exitus || className.startsWith("it/cnr/iasi/saks/groucho/");
-		exitus = exitus || className.startsWith("org/junit/");
-		exitus = exitus || className.startsWith("org/apache/maven/");
-		exitus = exitus || className.startsWith("org/eclipse/jdt/internal/junit/");
-		
-		if (!exitus) {
-			List<String> excludeClassesList = PropertyUtil.getInstance().getClassesToExcludeDuringTransformation();
-			for (Iterator<String> iterator = excludeClassesList.iterator(); iterator.hasNext() && !exitus;) {
-				String classToExclude = (String) iterator.next();
-				exitus = exitus || className.startsWith(classToExclude);
-
-			}
-		}
-		return exitus;
-	}
-
-	private boolean isClassNameIgnoredByCrochet(String className) {
-		 boolean exitus = net.jonbell.crij.instrument.Instrumenter.isIgnoredClass(className);
-		 exitus = exitus || net.jonbell.crij.instrument.Instrumenter.isIgnoredClassWithDummyMethods(className);
-		 return exitus;
-	}
-
 	/*
 	 * (non-Javadoc) It is invoked for every class loaded into the JVM. It
 	 * provides the byte array of the class as input to the method, which then
