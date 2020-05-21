e5632bb90b15d0b2f4adb1c3d2bd136439fd3f8e
Gulyx <guglielmo.deangelis@isti.cnr.it>
2020-05-21 12:00:26 +0200

    Improved local test that only serves to experiments mockito

--- groucho-lab/src/test/java/it/cnr/iasi/saks/groucho/lab/instrument/test/utils/OSCacheLocalTest.java
+++ groucho-lab/src/test/java/it/cnr/iasi/saks/groucho/lab/instrument/test/utils/OSCacheLocalTest.java
@@ -18,6 +18,7 @@
 package it.cnr.iasi.saks.groucho.lab.instrument.test.utils;
 
 import java.io.IOException;
+import java.util.Locale;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -29,6 +30,9 @@ import javax.servlet.http.HttpServletResponseWrapper;
 
 import com.opensymphony.oscache.web.filter.CacheFilter;
 import com.opensymphony.oscache.web.filter.ResponseContent;
+
+import it.cnr.iasi.saks.groucho.lab.instrument.test.experiments.oscache.SimpleOSCacheTest_IT;
+
 import com.opensymphony.oscache.base.Cache;
 import com.opensymphony.oscache.base.NeedsRefreshException;
 import com.opensymphony.oscache.web.ServletCacheAdministrator;
@@ -36,9 +40,14 @@ import com.opensymphony.oscache.web.ServletCacheAdministrator;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.internal.util.reflection.FieldSetter;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -47,49 +56,40 @@ public class OSCacheLocalTest{
 
 	@Test
 	public void doFilterOSCacheTest(){
-		String fooScope = "session";
-		
 		HttpServletRequest request = mock(HttpServletRequest.class);
 		HttpServletResponse response = mock(HttpServletResponse.class);
-		FilterChain chain = mock(FilterChain.class);
-				
-		HttpServletResponseWrapper responseWrapper = new HttpServletResponseWrapper(response); 
-		responseWrapper.setHeader("myfooHeader", "myfooValue");
-		
-		ResponseContent respContentMinValue = mock(ResponseContent.class);
-		when(respContentMinValue.getLastModified()).thenReturn(Long.MIN_VALUE);
-		
-//		Cache cache = mock(Cache.class);
-//		try {
-//			when(cache.getFromCache(any(), anyInt())).thenReturn(respContent);
-//		} catch (NeedsRefreshException e) {
-//			Assert.fail(e.getMessage());
-//		}
 
-		Cache cache = new Cache(true, false, false);
-		cache.putInCache("fooKey", respContentMinValue);
-//		try {
-//			when(cache.getFromCache(any(), anyInt())).thenReturn(respContent);
-//		} catch (NeedsRefreshException e) {
-//			Assert.fail(e.getMessage());
-//		}
+		DummyHttpServletResponseWrapper responseWrapper = new DummyHttpServletResponseWrapper(response); 		
+		Locale l = new Locale(SimpleOSCacheTest_IT.LOCALE_LANG_CONF);
+		responseWrapper.setLocale(l);
+		
+		FilterChain chain = mock(FilterChain.class);
+
+		ResponseContent respContent = mock(ResponseContent.class);
+		when(respContent.getLastModified()).thenReturn(Long.MIN_VALUE);
+
+		Cache cache = mock(Cache.class);
+		
+		try {
+			when(cache.getFromCache(any(), anyInt())).thenReturn(respContent);
+		} catch (NeedsRefreshException e) {
+			Assert.fail(e.getMessage());
+		}
 
 		ServletCacheAdministrator admin = mock(ServletCacheAdministrator.class);
 		lenient().when(admin.getCache(any(HttpServletRequest.class), anyInt())).thenReturn(cache);
-		lenient().when(admin.generateEntryKey(isNull(), any(HttpServletRequest.class), anyInt())).thenReturn("fooKey");
-		
+
 		ServletContext context = mock(ServletContext.class);
 		FilterConfig filterConfig = mock(FilterConfig.class);
 		when(filterConfig.getServletContext()).thenReturn(context);
-		when(filterConfig.getInitParameter(anyString())).thenReturn(fooScope);
-		
+		when(filterConfig.getInitParameter(anyString())).thenReturn(SimpleOSCacheTest_IT.SCOPE_CONF);
+
 		CacheFilter filter = new CacheFilter();
-		int hashcodeInit = filter.hashCode();
 		filter.init(filterConfig);
-				
+
 		try {
 			FieldSetter.setField(filter, CacheFilter.class.getDeclaredField("admin"), admin);
-						
+
 			filter.doFilter(request, responseWrapper, chain);
 		} catch (IOException e) {
 			Assert.fail(e.getMessage());
@@ -101,11 +101,7 @@ public class OSCacheLocalTest{
 			Assert.fail(e.getMessage());
 		}
 		
-		int hashcodeAfter = filter.hashCode();
-		System.out.println(hashcodeAfter);
-		
-		boolean condition = (hashcodeAfter == hashcodeInit) && (hashcodeAfter != OSCachePrinterInvivoTestClass.getLastComputedHashCode());
+		boolean condition = (responseWrapper.getLocale().getLanguage().equals(SimpleOSCacheTest_IT.LOCALE_LANG_CONF)) && (!responseWrapper.containsHeader(SimpleOSCacheTest_IT.HEADER_FOO_PARAM));
 		Assert.assertTrue(condition);
 	}
-
 }
