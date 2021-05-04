772759ea1cb3a819ad45c2a117aba6e0832a850e
Pelat-1 <nicolo.rogai@outlook.com>
2021-05-05 00:34:01 +0200

    Controller updated

--- groucho-lsh-rest/src/main/java/it/cnr/iasi/saks/groucho/lsh/rest/api/MarkApiController.java
+++ groucho-lsh-rest/src/main/java/it/cnr/iasi/saks/groucho/lsh/rest/api/MarkApiController.java
@@ -3,26 +3,22 @@ package it.cnr.iasi.saks.groucho.lsh.rest.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiParam;
 import it.cnr.iasi.saks.groucho.lsh.rest.impl.MarkApiImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.*;
 import javax.annotation.Generated;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 @Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-07-02T22:49:41.406+02:00")
 
 @Controller
+@Slf4j
 public class MarkApiController implements MarkApi {
-
-    private static final Logger log = LoggerFactory.getLogger(MarkApiController.class);
-
+    @Autowired
+    private MarkApiImpl markApiImpl;
     private final ObjectMapper objectMapper;
-
     private final HttpServletRequest request;
 
     @Autowired
@@ -31,14 +27,20 @@ public class MarkApiController implements MarkApi {
         this.request = request;
     }
 
-    public ResponseEntity<Boolean> markState(@ApiParam(value = "The Actual Internal Representation as a String of the Considered State." ,required=true )  @Valid @RequestBody String body) {
+    public ResponseEntity<Boolean> markState(
+            @ApiParam(value="The Actual Internal Representation as a String of the Considered State.", required=true)
+            @Valid @RequestBody String body) {
+        log.info("markState - input: {}", body);
         String accept = request.getHeader("Accept");
-        return MarkApiImpl.markState(body);
+        return markApiImpl.markState(body);
     }
 
-    public ResponseEntity<Boolean> markStateLSH(@ApiParam(value = "LSH String of Considered State",required=true) @PathVariable("stateStringLSH") String stateStringLSH) {
+    public ResponseEntity<Boolean> markStateLSH(
+            @ApiParam(value="LSH String of Considered State", required=true)
+            @PathVariable("stateStringLSH") String stateStringLSH) {
+        log.info("markStateLSH - input: {}", stateStringLSH);
         String accept = request.getHeader("Accept");
-        return MarkApiImpl.markStateLSH(stateStringLSH);
+        return markApiImpl.markStateLSH(stateStringLSH);
     }
 
 }
--- groucho-lsh-rest/src/main/java/it/cnr/iasi/saks/groucho/lsh/rest/api/ResetApiController.java
+++ groucho-lsh-rest/src/main/java/it/cnr/iasi/saks/groucho/lsh/rest/api/ResetApiController.java
@@ -2,23 +2,20 @@ package it.cnr.iasi.saks.groucho.lsh.rest.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.cnr.iasi.saks.groucho.lsh.rest.impl.ResetApiImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-
 import javax.annotation.Generated;
 import javax.servlet.http.HttpServletRequest;
 @Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-07-02T22:49:41.406+02:00")
 
 @Controller
+@Slf4j
 public class ResetApiController implements ResetApi {
-
-    private static final Logger log = LoggerFactory.getLogger(ResetApiController.class);
-
+    @Autowired
+    private ResetApiImpl resetApiImpl;
     private final ObjectMapper objectMapper;
-
     private final HttpServletRequest request;
 
     @Autowired
@@ -28,7 +25,8 @@ public class ResetApiController implements ResetApi {
     }
 
     public ResponseEntity<Boolean> resetStateObserver() {
+        log.info("resetStateObserver - input: NO");
         String accept = request.getHeader("Accept");
-        return ResetApiImpl.resetStateObserver();
+        return resetApiImpl.resetStateObserver();
     }
 }
