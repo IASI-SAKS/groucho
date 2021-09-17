e3037a4964d2941c82041cb94f72ecab76d77436
MorenaBarboni <morena.barboni@studenti.unicam.it>
2021-09-17 10:45:28 +0200

    Added parametrized DateTest

---
+++ groucho-lab/src/test/java/it/cnr/iasi/saks/groucho/lab/instrument/test/experiments/fastjson/test/DateTest4_indian.java
@@ -0,0 +1,59 @@
+/*Fastjson V 1.2.54*/
+package it.cnr.iasi.saks.groucho.lab.instrument.test.experiments.fastjson.test;
+
+import com.alibaba.fastjson.JSON;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class DateTest4_indian extends TestCase {
+
+    //TimeZone.getTimezone("Asia/Shangai") and Locale.CHINA
+    public void configure(TimeZone tz, Locale l){
+        JSON.defaultTimeZone = tz;
+        JSON.defaultLocale = l;
+    }
+
+    @Test
+    public void test_date() throws Exception {
+
+        Date date1 = JSON.parseObject("{\"gmtCreate\":\"2018-09-11T21:29:34+0530\"}", VO.class).getGmtCreate();
+
+        Assert.assertNotNull(date1);
+
+        Date date2 = JSON.parseObject("{\"gmtCreate\":\"2018-09-11T21:29:34+0500\"}", VO.class).getGmtCreate();
+        Date date3 = JSON.parseObject("{\"gmtCreate\":\"2018-09-11T21:29:34+0545\"}", VO.class).getGmtCreate();
+        Date date4 = JSON.parseObject("{\"gmtCreate\":\"2018-09-11T21:29:34+1245\"}", VO.class).getGmtCreate();
+        Date date5 = JSON.parseObject("{\"gmtCreate\":\"2018-09-11T21:29:34+1345\"}", VO.class).getGmtCreate();
+
+        long delta_2_1 = date2.getTime() - date1.getTime();
+        Assert.assertEquals(1800000, delta_2_1);
+
+        long delta_3_1 = date3.getTime() - date1.getTime();
+        Assert.assertEquals(-900000, delta_3_1);
+
+        long delta_4_3 = date4.getTime() - date3.getTime();
+        Assert.assertEquals(-25200000, delta_4_3);
+
+        long delta_5_4 = date5.getTime() - date4.getTime();
+        Assert.assertEquals(17100000, delta_5_4);
+    }
+
+    public static class VO {
+
+        private Date gmtCreate;
+
+        public Date getGmtCreate() {
+            return gmtCreate;
+        }
+
+        public void setGmtCreate(Date gmtCreate) {
+            this.gmtCreate = gmtCreate;
+        }
+
+    }
+}
---
+++ groucho-lab/src/test/java/it/cnr/iasi/saks/groucho/lab/instrument/test/experiments/fastjson/test/DateTest5_iso8601.java
@@ -0,0 +1,60 @@
+/*Fastjson V 1.2.54*/
+package it.cnr.iasi.saks.groucho.lab.instrument.test.experiments.fastjson.test;
+
+import com.alibaba.fastjson.JSON;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class DateTest5_iso8601 extends TestCase {
+
+    //TimeZone.getTimezone("Asia/Shangai") and Locale.CHINA
+    public void configure(TimeZone tz, Locale l){
+        JSON.defaultTimeZone = tz;
+        JSON.defaultLocale = l;
+    }
+
+    @Test
+    public void test_date() throws Exception {
+        Date date1 = JSON.parseObject("{\"gmtCreate\":\"2018-09-12\"}", VO.class).getGmtCreate();
+        Assert.assertNotNull(date1);
+        Date date2 = JSON.parseObject("{\"gmtCreate\":\"2018-09-12T15:10:19+00:00\"}", VO.class).getGmtCreate();
+        Date date3 = JSON.parseObject("{\"gmtCreate\":\"2018-09-12T15:10:19Z\"}", VO.class).getGmtCreate();
+        Date date4 = JSON.parseObject("{\"gmtCreate\":\"20180912T151019Z\"}", VO.class).getGmtCreate();
+        Date date5 = JSON.parseObject("{\"gmtCreate\":\"2018-09-12T15:10:19Z\"}", VO.class).getGmtCreate();
+        Date date6 = JSON.parseObject("{\"gmtCreate\":\"20180912\"}", VO.class).getGmtCreate();
+
+        long delta_2_1 = date2.getTime() - date1.getTime();
+        Assert.assertEquals(83419000, delta_2_1);
+
+        long delta_3_1 = date3.getTime() - date1.getTime();
+        Assert.assertEquals(83419000, delta_3_1);
+
+        long delta_4_3 = date4.getTime() - date3.getTime();
+        Assert.assertEquals(0, delta_4_3);
+
+        long delta_5_4 = date5.getTime() - date4.getTime();
+        Assert.assertEquals(0, delta_5_4);
+
+        long delta_6_1 = date6.getTime() - date1.getTime();
+        Assert.assertEquals(0, delta_6_1);
+    }
+
+    public static class VO {
+
+        private Date gmtCreate;
+
+        public Date getGmtCreate() {
+            return gmtCreate;
+        }
+
+        public void setGmtCreate(Date gmtCreate) {
+            this.gmtCreate = gmtCreate;
+        }
+
+    }
+}
