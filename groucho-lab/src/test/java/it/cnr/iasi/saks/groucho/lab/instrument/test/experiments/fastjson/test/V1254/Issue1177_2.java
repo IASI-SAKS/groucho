afc6fc04522485021ae3e5f33f47ed1753d516dc
MorenaBarboni <morena.barboni@studenti.unicam.it>
2021-09-27 17:44:54 +0200

    Issue1177_2 can use input generated from the Context

--- groucho-lab/src/test/java/it/cnr/iasi/saks/groucho/lab/instrument/test/experiments/fastjson/test/V1254/Issue1177_2.java
+++ groucho-lab/src/test/java/it/cnr/iasi/saks/groucho/lab/instrument/test/experiments/fastjson/test/V1254/Issue1177_2.java
@@ -1,28 +1,53 @@
 /*FastJSON V 1.2.54*/
 package it.cnr.iasi.saks.groucho.lab.instrument.test.experiments.fastjson.test.V1254;
 
+import clojure.lang.Obj;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.JSONPath;
 import com.alibaba.fastjson.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import junit.framework.TestCase;
+import org.apache.commons.lang.StringUtils;
+import com.jayway.jsonpath.*;
+import org.junit.Assert;
 
+import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by wenshao on 05/05/2017.
+/*
+ * This class is a re-implementation of the original Unit Test:
+ * com.alibaba.json.bvt.issue_1100.Issue1177_2
+ * distributed with Fastjson 1.2.54
  */
-public class Issue1177_2 extends TestCase {
+public class Issue1177_2 {
+
+    String text;
+    String key;
+
+    public Issue1177_2() {
+        this.text = "{\"a\":{\"x\":\"y\"},\"b\":{\"x\":\"y\"}}";
+        this.key = "x";
+    }
+
+    public void configure(String text, String key){
+        this.text = text;
+        this.key = key;
+        System.out.println("... configuration done.");
+    }
 
     public void test_for_issue() throws Exception {
-        String text = "{\"a\":{\"x\":\"y\"},\"b\":{\"x\":\"y\"}}";
+        String text = this.text;
+
         Map<String, Model> jsonObject = JSONObject.parseObject(text, new TypeReference<Map<String, Model>>(){});
-        System.out.println(JSON.toJSONString(jsonObject));
-        System.out.println(text);
-        String jsonpath = "$..x";
+
+        String jsonpath = "$.."+key;
         String value="y2";
-        JSONPath.set(text, jsonpath, value);
-        assertEquals("{\"a\":{\"x\":\"y2\"},\"b\":{\"x\":\"y2\"}}}", JSON.toJSONString(text));
+        JSONPath.set(jsonObject, jsonpath, value);
+
+        Object expectedObject = JsonPath.parse(text).set(jsonpath, value).json();
+
+        Assert.assertEquals(JSON.toJSONString(expectedObject), JSON.toJSONString(jsonObject));
     }
 
     public static class Model {
