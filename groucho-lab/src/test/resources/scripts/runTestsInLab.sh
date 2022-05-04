e5cf00d5f9cc0830218c3033269eac6ad9149308
MorenaBarboni <morena.barboni@studenti.unicam.it>
2022-05-04 09:04:41 +0200

    Test refactoring

--- groucho-lab/src/test/resources/scripts/runTestsInLab.sh
+++ groucho-lab/src/test/resources/scripts/runTestsInLab.sh
@@ -5,18 +5,18 @@ TEST=""
 start=$(date)
 echo "Running script in: " $PWD
 echo "Start time : " $start
-echo "Start time : " $start &>>CassiaInLab-$VERSION.txt
+echo "Start time : " $start &>>OriginalTestInLab-$VERSION.txt
 
 for i in {1..10000}
 do 
-echo " " &>>CassiaInLab-$VERSION.txt
-echo "Run N.$i" &>>CassiaInLab-$VERSION.txt
+echo " " &>>OriginalTestInLab-$VERSION.txt
+echo "Run N.$i" &>>OriginalTestInLab-$VERSION.txt
 echo "Run N.$i" 
-mvn -Dtest=$TEST  -Dmaven.test.failure.ignore=true test -DreuseForks=false -DuseUnlimitedThreads=false &>>CassiaInLab-$VERSION.txt
+mvn -Dtest="$TEST"  -Dmaven.test.failure.ignore=true test -DreuseForks=false -DuseUnlimitedThreads=false &>>OriginalTestInLab-$VERSION.txt
 done
 
 end=$(date)
-echo "End time : " $end &>>CassiaInLab-$VERSION.txt
+echo "End time : " $end &>>OriginalTestInLab-$VERSION.txt
 echo "End time : " $end
 
 exit 0
