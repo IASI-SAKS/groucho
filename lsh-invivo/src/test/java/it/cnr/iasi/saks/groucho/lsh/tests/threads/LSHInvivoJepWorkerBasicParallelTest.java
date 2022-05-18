b8432aa8fa5f2dc80d2149403d8eb23687eabfe3
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2022-05-18 14:35:32 +0200

    Improved test by stopping the JEP Worker thread

--- lsh-invivo/src/test/java/it/cnr/iasi/saks/groucho/lsh/tests/threads/LSHInvivoJepWorkerBasicParallelTest.java
+++ lsh-invivo/src/test/java/it/cnr/iasi/saks/groucho/lsh/tests/threads/LSHInvivoJepWorkerBasicParallelTest.java
@@ -1,3 +1,20 @@
+/* 
+ * This file is part of the GROUCHO project.
+ * 
+ * GROUCHO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * GROUCHO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GROUCHO.  If not, see <https://www.gnu.org/licenses/>
+ *
+ */
 package it.cnr.iasi.saks.groucho.lsh.tests.threads;
 
 import static org.junit.Assert.assertEquals;
@@ -7,12 +24,11 @@ import java.util.List;
 
 import org.junit.Test;
 
-import it.cnr.iasi.saks.groucho.lsh.exceptions.LSHException;
 import it.cnr.iasi.saks.groucho.lsh.jep.threads.LSHInvivoJepWorker;
 
 public class LSHInvivoJepWorkerBasicParallelTest {
 	
-	private final static int NUMBER_OF_CLENTS = 30;		
+	private final static int NUMBER_OF_CLENTS = 30;			
 
 	@Test
 	public void simpleParallelTest() {
@@ -57,8 +73,9 @@ public class LSHInvivoJepWorkerBasicParallelTest {
 			
 			counterToForceExit--;
 		}
-		
+
 		assertEquals(NUMBER_OF_CLENTS,numberOfRequestersCompleted);
+		jepWorker.stopWorker();
 	}
 
 }
