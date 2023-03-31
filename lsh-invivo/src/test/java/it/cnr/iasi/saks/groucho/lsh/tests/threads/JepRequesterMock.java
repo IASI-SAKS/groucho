46690fb0f8add1f22f293d34b4d2e00187067760
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2023-03-31 13:48:45 +0200

    Improved the implementation of the Mock

--- lsh-invivo/src/test/java/it/cnr/iasi/saks/groucho/lsh/tests/threads/JepRequesterMock.java
+++ lsh-invivo/src/test/java/it/cnr/iasi/saks/groucho/lsh/tests/threads/JepRequesterMock.java
@@ -22,50 +22,54 @@ import it.cnr.iasi.saks.groucho.lsh.jep.threads.LSHInvivoJepWorker;
 
 public class JepRequesterMock implements Runnable {
 	private LSHInvivoJepWorker worker;
-	private volatile boolean completed; 
-	
+	private volatile boolean completed;
+
+	private static final int MAX_RUNS = 5;
+
 	public JepRequesterMock(LSHInvivoJepWorker worker) {
 		this.worker = worker;
-		
+
 		this.completed = false;
 	}
-	
+
 	@Override
 	public void run() {
-		long millis = ((long)(Math.random()*10))*1000;
+		for (int i = 0; i < MAX_RUNS; i++) {
+			long millis = ((long) (Math.random() * 10)) * 1000;
 //		System.out.println("Sleeping for:"+millis);
 
-		int typeOfRequest = ((int)(Math.random()*3))%3;
+			int typeOfRequest = ((int) (Math.random() * 3)) % 3;
 //		System.out.println("Request:"+typeOfRequest);
-		
-		try {
-			Thread.sleep(millis);
-			switch (typeOfRequest) {
-			case 0:
-				this.worker.isStateUnknown("carvedState");				
-				break;
-			case 1:
-				this.worker.markState("carvedState");
-				break;
-			case 2:
-				this.worker.resetStateObserver();				
-				break;
-			default:
-				this.worker.isStateUnknown("carvedState");				
-				break;
+
+			try {
+				Thread.sleep(millis);
+				switch (typeOfRequest) {
+				case 0:
+					this.worker.isStateUnknown("carvedState");
+					break;
+				case 1:
+					this.worker.markState("carvedState");
+					break;
+				case 2:
+					this.worker.resetStateObserver();
+					break;
+				default:
+					this.worker.isStateUnknown("carvedState");
+					break;
+				}
+				this.completed = true;
+			} catch (InterruptedException e) {
+				this.completed = false;
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (LSHException e) {
+				this.completed = false;
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			this.completed = true;
-		} catch (InterruptedException e) {
-			this.completed = false;
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (LSHException e) {
-			this.completed = false;
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
-	
+
 	public synchronized boolean isRequestCompleted() {
 		return this.completed;
 	}
