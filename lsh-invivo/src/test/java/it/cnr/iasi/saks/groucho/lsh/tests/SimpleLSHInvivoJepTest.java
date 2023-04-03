051a8cd5a0ea4609d7a2855b9634aa30da34282a
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2023-04-03 18:17:37 +0200

    Test Improved

--- lsh-invivo/src/test/java/it/cnr/iasi/saks/groucho/lsh/tests/SimpleLSHInvivoJepTest.java
+++ lsh-invivo/src/test/java/it/cnr/iasi/saks/groucho/lsh/tests/SimpleLSHInvivoJepTest.java
@@ -19,6 +19,7 @@ package it.cnr.iasi.saks.groucho.lsh.tests;
 
 import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Ignore;
 
 import it.cnr.iasi.saks.groucho.lsh.exceptions.LSHException;
 import it.cnr.iasi.saks.groucho.lsh.jep.LSHInvivoJep;
@@ -29,6 +30,8 @@ public class SimpleLSHInvivoJepTest {
 	private final String DUMMY_CARVED_STATE = "{dummy-->[]%fieldBoolean-->true%fieldChar-->d%fieldInt-->-1%fieldObject-->[]%fieldString-->deafult%otherDummy-->[]}";
 	private final String DUMMY_CARVED_STATE_5 = "{dummy-->{dc-->{dummy-->{dc-->{dummy-->[]%fieldBoolean-->true%fieldChar-->d%fieldInt-->-1%fieldObject-->[]%fieldString-->deafult%otherDummy-->[]}%myList-->{elementData-->[çççç]%size-->5}%mySimpleState-->999%v-->{capacityIncrement-->0%elementCount-->5%elementData-->[10ç20ç30ç40ç50]}%words-->{capacityIncrement-->0%elementCount-->2%elementData-->[FooçBoo]}}%fieldBoolean-->true%fieldChar-->d%fieldInt-->-1%fieldObject-->{}%fieldString-->deafult%otherDummy-->{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}}%myList-->{elementData-->[{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}ç{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}ç{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}ç{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}ç{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}]%size-->5}%mySimpleState-->999%v-->{capacityIncrement-->0%elementCount-->5%elementData-->[10ç20ç30ç40ç50]}%words-->{capacityIncrement-->0%elementCount-->2%elementData-->[FooçBoo]}}%fieldBoolean-->true%fieldChar-->d%fieldInt-->-1%fieldObject-->{}%fieldString-->deafult%otherDummy-->{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}}";
 
+	private final String FAKE_LSH_STRING = "9832ndnkjsao8792932jdlaksnlk7aè";	
+	
 	public SimpleLSHInvivoJepTest() throws LSHException{		
 		System.out.println(System.getProperty("java.library.path"));
 
@@ -39,7 +42,7 @@ public class SimpleLSHInvivoJepTest {
 	
 	@Test
 	public void basicTest() throws LSHException{
-		this.obs.markState(DUMMY_CARVED_STATE_5);
+		this.obs.markState(DUMMY_CARVED_STATE);
 		boolean condition = this.obs.isStateUnknown(DUMMY_CARVED_STATE_5);
 //		this.obs.detachJEP();
 		
@@ -48,8 +51,55 @@ public class SimpleLSHInvivoJepTest {
 
 	@Test
 	public void anotherBasicTest() throws LSHException{
-		boolean condition = this.obs.isStateUnknown(DUMMY_CARVED_STATE);
+		boolean condition = this.obs.isStateUnknown(FAKE_LSH_STRING);
 //		this.obs.detachJEP();
-		Assert.assertTrue(condition);
+
+// ---------------------------------------------------
+		Assert.assertEquals(true, condition);
+// ---------------------------------------------------
+//		Assert.assertTrue(condition);
+// ---------------------------------------------------
 	}
+	
+//	***********************************************************************************
+//	***********************************************************************************
+	
+	@Test
+	public void knownStateTest() throws LSHException {
+		this.obs.markState(DUMMY_CARVED_STATE_5);
+		boolean condition = this.obs.isStateUnknown(DUMMY_CARVED_STATE_5);
+		Assert.assertFalse(condition);
+	}
+
+	@Test
+	public void unknownStateTest() throws LSHException {
+		this.obs.markState(DUMMY_CARVED_STATE_5);
+
+		this.obs.resetStateObserver();
+
+		boolean condition = this.obs.isStateUnknown(DUMMY_CARVED_STATE_5);
+
+// ---------------------------------------------------
+		Assert.assertEquals(true, condition);
+// ---------------------------------------------------
+//		Assert.assertTrue(condition);
+// ---------------------------------------------------
+	}
+
+	@Ignore
+	@Test
+	public void unknownStateMultipleChecksTest() throws LSHException {
+		this.obs.resetStateObserver();
+		
+		for (int i=0; i<5; i++){
+			boolean condition = this.obs.isStateUnknown(DUMMY_CARVED_STATE_5);
+
+// ---------------------------------------------------
+			Assert.assertEquals(true, condition);
+// ---------------------------------------------------
+//			Assert.assertTrue(condition);
+// ---------------------------------------------------
+		}
+	}
+		
 }
