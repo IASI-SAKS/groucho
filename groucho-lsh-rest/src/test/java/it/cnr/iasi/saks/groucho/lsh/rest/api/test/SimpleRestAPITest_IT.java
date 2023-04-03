08770d4a5b14ede786114e5c1e5379b82e611114
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2023-04-03 13:03:08 +0200

    Added a class template for integration testing

---
+++ groucho-lsh-rest/src/test/java/it/cnr/iasi/saks/groucho/lsh/rest/api/test/SimpleRestAPITest_IT.java
@@ -0,0 +1,40 @@
+package it.cnr.iasi.saks.groucho.lsh.rest.api.test;
+
+import javax.xml.ws.Response;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import it.cnr.iasi.saks.groucho.lsh.rest.api.impl.IsunknownApiController;
+
+public class SimpleRestAPITest_IT {
+
+	private final String DUMMY_CARVED_STATE = "{dummy-->[]%fieldBoolean-->true%fieldChar-->d%fieldInt-->-1%fieldObject-->[]%fieldString-->deafult%otherDummy-->[]}";
+	private final String DUMMY_CARVED_STATE_5 = "{dummy-->{dc-->{dummy-->{dc-->{dummy-->[]%fieldBoolean-->true%fieldChar-->d%fieldInt-->-1%fieldObject-->[]%fieldString-->deafult%otherDummy-->[]}%myList-->{elementData-->[çççç]%size-->5}%mySimpleState-->999%v-->{capacityIncrement-->0%elementCount-->5%elementData-->[10ç20ç30ç40ç50]}%words-->{capacityIncrement-->0%elementCount-->2%elementData-->[FooçBoo]}}%fieldBoolean-->true%fieldChar-->d%fieldInt-->-1%fieldObject-->{}%fieldString-->deafult%otherDummy-->{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}}%myList-->{elementData-->[{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}ç{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}ç{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}ç{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}ç{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}]%size-->5}%mySimpleState-->999%v-->{capacityIncrement-->0%elementCount-->5%elementData-->[10ç20ç30ç40ç50]}%words-->{capacityIncrement-->0%elementCount-->2%elementData-->[FooçBoo]}}%fieldBoolean-->true%fieldChar-->d%fieldInt-->-1%fieldObject-->{}%fieldString-->deafult%otherDummy-->{fieldBooleanOther-->false%fieldCharOther-->c%fieldEnum-->three%fieldIntOther-->88%fieldStringOther-->thisIsFoo}}";
+
+	private final String FAKE_LSH_STRING = "9832ndnkjsao8792932jdlaksnlk7aè";
+
+	private MockMvc mockMvc;
+	private Response response;
+	 
+//	@Before
+//    public void setUp() {
+//        mockMvc = MockMvcBuilders.standaloneSetup(IsunknownApiController.class).build();
+//    }
+//	
+//	 @Test
+//	    public void testHome() throws Exception {
+//	        mockMvc.perform(MockMvcRequestBuilders.get("/isunknown").param("value", FAKE_LSH_STRING))
+//	                .andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string(Boolean.TRUE.toString()));
+//	 
+//	 }
+
+	@Test
+    public void warningMessage() throws Exception {
+		System.err.println("****************************************\n********** REMEMBER TO IMPLEMENT THE INTEGRATION TESTS IN THE CLASS: " + SimpleRestAPITest_IT.class.getCanonicalName() + " **********\n****************************************");
+	}
+}
